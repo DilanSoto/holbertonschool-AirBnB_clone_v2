48f06cca672f0b594ec6c4fa7882de9511486e2e
Dilan Soto Ortiz <144055652+DilanSoto@users.noreply.github.com>
2024-03-29 22:54:28 -0400

    states update

--- models/state.py
+++ models/state.py
@@ -19,11 +19,3 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
     
-    @property
-    def cities(self):
-        city_list = []
-        for city in list(models.storage.all(City).values()):
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
-    
