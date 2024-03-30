079c535d3da5e3fe0211945ab13803d63214a3df
comidaparallevar <lyanosorio98@gmail.com>
2024-03-29 23:07:35 -0400

    added arg parsing to do_create function

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 """ Module for testing file storage"""
 import unittest
+import os
 from models.base_model import BaseModel
 from models import storage
-import os
 
 
-class test_fileStorage(unittest.TestCase):
+class TestFileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
     def setUp(self):
         """ Set up test environment """
         del_list = []
-        for key in storage._FileStorage__objects.keys():
+        for key in storage.all():
             del_list.append(key)
         for key in del_list:
-            del storage._FileStorage__objects[key]
+            del storage.all()[key]
 
     def tearDown(self):
         """ Remove storage file at end of tests """
         try:
             os.remove('file.json')
-        except:
+        except FileNotFoundError:
             pass
 
     def test_obj_list_empty(self):
@@ -30,48 +30,48 @@ class test_fileStorage(unittest.TestCase):
 
     def test_new(self):
         """ New object is correctly added to __objects """
-        new = BaseModel()
+        BaseModel()
+        obj = None  # Define the variable 'obj' before the loop
         for obj in storage.all().values():
             temp = obj
         self.assertTrue(temp is obj)
 
     def test_all(self):
         """ __objects is properly returned """
-        new = BaseModel()
+        BaseModel()
         temp = storage.all()
         self.assertIsInstance(temp, dict)
 
     def test_base_model_instantiation(self):
         """ File is not created on BaseModel save """
-        new = BaseModel()
+        BaseModel()
         self.assertFalse(os.path.exists('file.json'))
 
     def test_empty(self):
         """ Data is saved to file """
-        new = BaseModel()
-        thing = new.to_dict()
-        new.save()
-        new2 = BaseModel(**thing)
+        BaseModel()
+        thing = BaseModel().to_dict()
+        BaseModel().save()
         self.assertNotEqual(os.path.getsize('file.json'), 0)
 
     def test_save(self):
         """ FileStorage save method """
-        new = BaseModel()
+        BaseModel()
         storage.save()
         self.assertTrue(os.path.exists('file.json'))
 
     def test_reload(self):
         """ Storage file is successfully loaded to __objects """
-        new = BaseModel()
+        BaseModel()
         storage.save()
         storage.reload()
         for obj in storage.all().values():
             loaded = obj
-        self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
+        self.assertEqual(BaseModel().to_dict()['id'], loaded.to_dict()['id'])
 
     def test_reload_empty(self):
         """ Load from an empty file """
-        with open('file.json', 'w') as f:
+        with open('file.json', 'w', encoding='utf-8'):  # Specify the encoding parameter
             pass
         with self.assertRaises(ValueError):
             storage.reload()
@@ -82,23 +82,23 @@ class test_fileStorage(unittest.TestCase):
 
     def test_base_model_save(self):
         """ BaseModel save method calls storage save """
-        new = BaseModel()
-        new.save()
+        BaseModel().save()
         self.assertTrue(os.path.exists('file.json'))
 
     def test_type_path(self):
         """ Confirm __file_path is string """
-        self.assertEqual(type(storage._FileStorage__file_path), str)
+        self.assertEqual(type(storage._FileStorage__file_path), str)  # Access the protected member using the appropriate class name
 
     def test_type_objects(self):
         """ Confirm __objects is a dict """
         self.assertEqual(type(storage.all()), dict)
 
+
     def test_key_format(self):
         """ Key is properly formatted """
-        new = BaseModel()
-        _id = new.to_dict()['id']
-        for key in storage.all().keys():
+        BaseModel()
+        _id = BaseModel().to_dict()['id']
+        for key in storage.all():
             temp = key
         self.assertEqual(temp, 'BaseModel' + '.' + _id)
 
