1cc0e6aec673f3e002785ffc3c97e67b7b6eab02
Dilan Soto Ortiz <144055652+DilanSoto@users.noreply.github.com>
2024-03-29 22:43:21 -0400

    one line space

--- models/state.py
+++ models/state.py
@@ -18,6 +18,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
+    
     @property
     def cities(self):
         city_list = []
